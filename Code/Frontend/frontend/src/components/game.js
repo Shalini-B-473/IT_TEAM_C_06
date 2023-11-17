572fb847f17c950bdf850324ab8f5c76fca3de64
student <student@mcc.com>
2023-11-17 09:33:54 +0530

    Update game.js

--- Code/Frontend/frontend/src/components/game.js
+++ Code/Frontend/frontend/src/components/game.js
@@ -17,29 +17,28 @@ export default class Game extends React.Component {
       sourceSelection: -1,
       status: '',
       turn: 'white',
-      val: '',
-      place: ["a9", "b9", "c9", "d9", "e9", "f9", "g9", "h9", "i9", "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "i8", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "i6", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "i5", "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "i4", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "i3", "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "i1"],
-      coins: ["R", "N", "B", "Q", "K", "Q", "B", "N", "R", "", "R", "", "", "", "", "", "B", "", "P", "P", "P", "P", "P", "P", "P", "P", "P", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "P", "P", "P", "P", "P", "P", "P", "P", "P", "", "B", "", "", "", "", "", "R", "", "R", "N", "B", "Q", "K", "Q", "B", "N", "R"],
-      counter: 0,
-      sno: 1,
-      tableData: [],
+      val:'',
+      place:["a9","b9","c9","d9","e9","f9","g9","h9","i9","a8","b8","c8","d8","e8","f8","g8","h8","i8","a7","b7","c7","d7","e7","f7","g7","h7","i7","a6","b6","c6","d6","e6","f6","g6","h6","i6","a5","b5","c5","d5","e5","f5","g5","h5","i5","a4","b4","c4","d4","e4","f4","g4","h4","i4","a3","b3","c3","d3","e3","f3","g3","h3","i3","a2","b2","c2","d2","e2","f2","g2","h2","i2","a1","b1","c1","d1","e1","f1","g1","h1","i1"],
+      coins:["R","N","B","Q","K","Q","B","N","R","","R","","","","","","B","","P","P","P","P","P","P","P","P","P","","","","","","","","","","","","","","","","","","","","","","","","","","","","P","P","P","P","P","P","P","P","P","","B","","","","","","R","","R","N","B","Q","K","Q","B","N","R"],
+      counter:0,
+      sno:1,
+      tableData : [],
       prevTime: null,
       currentTime: new Date(),
-      timeDifference: null,
-      // whiteTime : 0,
-      // blackTime : 0
+      currenttimeDifference: null,
+      prevtimeDifference: null
     }
   }
-
   updateTime() {
     const currentTime = new Date();
     const prevTime = this.state.currentTime;
-    const timeDifference = prevTime ? Math.abs(currentTime - prevTime) / 1000 : null;
-
+    const currenttimeDifference = prevTime ? Math.abs(currentTime - prevTime) / 1000 : null;
+    const prevtimeDifference = this.state.currenttimeDifference
     this.setState({
       prevTime,
       currentTime,
-      timeDifference,
+      currenttimeDifference,
+      prevtimeDifference
     });
   }
 
@@ -63,8 +62,8 @@ export default class Game extends React.Component {
       }
       return
     }
-
     this.updateTime();
+
     squares[this.state.sourceSelection].style = { ...squares[this.state.sourceSelection].style, backgroundColor: "" };
 
     if (squares[i] && squares[i].player === this.state.player) {
@@ -79,47 +78,48 @@ export default class Game extends React.Component {
       const blackFallenSoldiers = [];
       const isDestEnemyOccupied = Boolean(squares[i]);
       const isMovePossible = squares[this.state.sourceSelection].isMovePossible(this.state.sourceSelection, i, isDestEnemyOccupied);
-
+      
       //const className = isMovePossible.className;
       //console.log("Piece Class Name : " , className);
-
-      let coinsCopy = { ...this.state.coins };
+      
+      let coinsCopy = {...this.state.coins};
       coinsCopy[i] = this.state.coins[parseInt(this.state.sourceSelection)]; //new src
       coinsCopy[parseInt(this.state.sourceSelection)] = ""; //prev src null
-      if (squares[i] !== null) {
-        this.setState({
-          val: this.state.coins[parseInt(this.state.sourceSelection)] + this.state.place[parseInt(this.state.sourceSelection)] + "X" + this.state.place[parseInt(i)],
-          coins: coinsCopy
+      if(squares[i] !== null)
+        {
+          this.setState({
+                val : this.state.coins[parseInt(this.state.sourceSelection)]+this.state.place[parseInt(this.state.sourceSelection)]+"X"+this.state.place[parseInt(i)],
+               coins : coinsCopy 
         })
-      }
-      else {
-        this.setState({
-          val: this.state.coins[parseInt(this.state.sourceSelection)] + this.state.place[parseInt(this.state.sourceSelection)] + this.state.place[parseInt(i)],
-          coins: coinsCopy
+        }
+        else{
+          this.setState({
+                val : this.state.coins[parseInt(this.state.sourceSelection)]+this.state.place[parseInt(this.state.sourceSelection)]+this.state.place[parseInt(i)],
+                coins : coinsCopy 
         })
-      }
-
-
+        }
+        
+      
       if (isMovePossible) {
         //console.log("Squares : " + squares + " " + squares[i].getClass().getSimpleName())
         //const pieceClassName = squares[i].constructor.name;
         //console.log("Selected piece : " + {pieceClassName})
         if (squares[i] !== null) {
-
+          
           if (squares[i].player === 1) {
             whiteFallenSoldiers.push(squares[i]);
           }
           else {
             blackFallenSoldiers.push(squares[i]);
           }
-
+          
         }
 
         squares[i] = squares[this.state.sourceSelection];
         squares[this.state.sourceSelection] = null;
-
-
-
+        
+        
+        
         const isCheckMe = this.isCheckForPlayer(squares, this.state.player)
 
         if (isCheckMe) {
@@ -149,7 +149,7 @@ export default class Game extends React.Component {
         });
       }
     }
-
+    
   }
   componentDidUpdate(prevProps, prevState) {
     console.log(this.state.val)
@@ -165,18 +165,35 @@ export default class Game extends React.Component {
         this.setState({
           counter: 1
         });
-      }
+      } 
       else {
         axios.post("http://localhost:5000/Update", this.state)
           .then(result => {
             console.log(result);
             if (result.data && result.data.data) {
               // Update the state with the updated data received from the server
-              this.setState({
-                tableData: [...this.state.tableData, result.data.data],
-                counter: 0,
-                sno: this.state.sno + 1,
-              });
+              const updatedData = Array.isArray(result.data.data)
+        ? result.data.data.map(item => ({
+            ...item,
+            prevtimeDifference: this.state.prevtimeDifference,
+            currenttimeDifference: this.state.currenttimeDifference
+          }))
+        : [
+            {
+              ...result.data.data,
+              prevtimeDifference: this.state.prevtimeDifference,
+              currenttimeDifference: this.state.currenttimeDifference
+            }
+          ];
+
+          this.setState(prevState => ({
+            tableData: [
+              ...prevState.tableData,
+              ...updatedData
+            ],
+            counter: 0,
+            sno: prevState.sno + 1,
+          }));
             } else {
               console.error("Received data is not an array:", result.data);
             }
@@ -184,44 +201,12 @@ export default class Game extends React.Component {
           .catch(error => {
             console.log(error);
           });
-        // this.setState({
-        //   counter: 0,
-        //   sno: this.state.sno +1
-        // });
+       
       }
 
-      // axios.get("http://localhost:5000/FetchData/?sno="+this.state.sno)
-      //     .then(response => {
-      //       this.setState({
-      //         tableData: response.data,
-      //         counter: 0,
-      //         sno: this.state.sno + 1,
-      //       });
-      //     })
-      //     .catch(error => {
-      //       console.log(error);
-      //     });
+     
 
-     /* if (this.state.turn !== prevState.turn) {
-        const currentTime = new Date().getTime();
-        const timeDiff = currentTime - this.state.prevTime;
-  
-        if (this.state.turn === 'white') {
-          this.setState(prevState => ({
-            
-            whiteTime: prevState.whiteTime + timeDiff,
-            prevTime: currentTime,
-          }));
-        } else {
-          this.setState(prevState => ({
-            blackTime: prevState.blackTime + timeDiff,
-            prevTime: currentTime,
-          }));
-        }
-      }*/
     }
-
-
   }
 
   renderTable() {
@@ -230,7 +215,7 @@ export default class Game extends React.Component {
       return null; // or handle it accordingly
     }
     return (
-      <table className=' table table-light table-bordered table-hover' style={{ width: "70%", textAlign: "center" }}>
+      <table className=' table table-light table-bordered table-hover'style={{width : "70%" , textAlign :  "center"}}> 
         <thead>
           <tr>
             <th>Sno</th>
@@ -245,18 +230,16 @@ export default class Game extends React.Component {
             <tr key={row.sno}>
               <td>{row.sno}</td>
               <td>{row.white}</td>
-              {/* <td>{this.state.whiteTime}</td> */}
-              <td></td>
+              <td>{row.prevtimeDifference}</td>
               <td>{row.black}</td>
-              {/* <td>{this.state.blackTime}</td> */}
-              <td></td>
+              <td>{row.currenttimeDifference}</td>
             </tr>
           ))}
         </tbody>
       </table>
     );
   }
-
+  
 
 
   getKingPosition(squares, player) {
@@ -286,8 +269,8 @@ export default class Game extends React.Component {
 
     return (
       <div>
-        <div className="game" style={{ marginTop: "10px" }} >
-          <h2 style={{ textAlign: "left" }}>9x9 Board Game</h2>
+        <div className="game">
+          <h2 style={{textAlign:"left"}}>9x9 Board Game</h2>
           <div className="game-board">
             <Board
               squares={this.state.squares}
@@ -297,16 +280,18 @@ export default class Game extends React.Component {
           <div className="game-info">
             <h3>Turn</h3>
             <div id="player-turn-box" style={{ backgroundColor: this.state.turn }}>
+
             </div>
             <div>
-              <div >{this.state.val}</div>
+            <div >{this.state.val}</div>
             </div>
             <div className="game-status">{this.state.status}</div>
-
-            <div>
+           
+            {/* <div>
               <h3>Time Difference:</h3>
-              <p>{this.state.timeDifference !== null ? `${this.state.timeDifference} seconds` : 'N/A'}</p>
-            </div>
+              <p>{this.state.currenttimeDifference !== null ? `${this.state.currenttimeDifference} seconds` : 'N/A'}</p>
+              <p>{this.state.prevtimeDifference !== null ? `${this.state.prevtimeDifference} seconds` : 'N/A'}</p>
+            </div> */}
 
             <div className="fallen-soldier-block">
               <p className='display-6'>Fallen Soldiers Block</p>
@@ -319,12 +304,12 @@ export default class Game extends React.Component {
 
           </div>
           <div className='container-fluid'>
-            <p className="display-6 .ml-" >Movement Track Sheet</p>
+            <p className="display-6">Movement Track Sheet</p>
             {this.renderTable()}
           </div>
         </div>
 
-
+  
       </div>
 
     );
